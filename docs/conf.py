ecf3840a0158d1fd83a5de6049e903e087e25b31
Vlad Lyalin <Guitaricet@gmail.com>
2020-01-06 16:52:57 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -45,8 +45,8 @@ source_suffix = ['.rst', '.md']
 master_doc = 'index'
 
 # General information about the project.
-project = u'Read the Docs Template'
-copyright = u'2014, Read the Docs'
+project = u'Text Machine Wiki'
+copyright = u'2019, Text Machine Lab'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
