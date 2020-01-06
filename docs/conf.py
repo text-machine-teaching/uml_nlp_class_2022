c4be4cbdf91d2071d95478d948181d4407a37e39
Vlad Lyalin <Guitaricet@gmail.com>
2020-01-06 15:16:36 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -28,13 +28,15 @@ import os
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = [
+    'recommonmark',  # markdown support
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
 # The suffix of source filenames.
-source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
 
 # The encoding of source files.
 #source_encoding = 'utf-8-sig'
